2e933b9d8e81bac44bc5090fd1278c1798d4066c
ktj555 <ktj91119574@gmail.com>
2023-10-12 16:44:58 +0900

    r

--- udf_version2/define_execute_on_loading.cpp
+++ udf_version2/define_execute_on_loading.cpp
@@ -8,4 +8,7 @@ DEFINE_EXECUTE_ON_LOADING(set_load, libname)
 {
 	Set_User_Scalar_Name(enthalpy, "Modified_enthalpy");
 	Set_User_Scalar_Name(temp_s, "solid_temperature");
+
+	Set_User_Memory_Name(saturation, "Saturation");
+	Set_User_Memory_Name(fluid_temperature, "Fluid_temperature");
 }
